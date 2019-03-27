822171c713c6fc7947b836456b0578c5b56e861f
Drake Bott <bott.da@gmail.com>
2019-03-27 15:33:20 -0600

    Remove console logs

--- utils/fetchStats.js
+++ utils/fetchStats.js
@@ -1,16 +1,21 @@
 const fetch = require('node-fetch');
+const utf8 = require('utf8');
 
 function fetchTeamStats(team) {
-  return team.map(async player => {
-    const tag = player.replace('#', '-');
-    const res = await fetch(`https://ow-api.com/v1/stats/pc/us/${tag}/profile`);
-    const stats = await res.json();
-    const rating = stats.private ? 'Private' : stats.rating;
-    return {
-      player,
-      rating,
-    };
-  });
+  return Promise.all(
+    team.map(async player => {
+      const tag = player.replace('#', '-').split(' ')[0];
+      const res = await fetch(
+        utf8.encode(`https://ow-api.com/v1/stats/pc/us/${tag}/profile`),
+      );
+      const stats = await res.json();
+      const rating = stats.rating || 'Private';
+      return {
+        player,
+        rating,
+      };
+    }),
+  );
 }
 
 module.exports = { fetchTeamStats };
