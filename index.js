ac6effd6a163f2af3a57b12bec4312498d648d97
Drake Bott <bott.da@gmail.com>
2019-03-27 11:21:37 -0600

    OD matchup takes argument of what match they want

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const teams = {
+  '5280 ELITE': '5280 Elite',
+  '5280 ACADEMY': '5280 Academy',
+  '5280 PINK (ACADEMY)': '5280 Pink',
+  '5280 BLUE': '5280 Blue',
+  '5280 RED': '5280 Red',
+  '5280 DARK': '5280 Dark',
+  GENERAL: '5280 Elite',
+};
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -10,9 +20,19 @@ client.on('message', message => {
   if (message.author === client.user) {
     return;
   }
-  if (message.content === "!OD matchup") {
-    message.channel.send("OPEN DIV STATS");
+  if (message.content.startsWith('!OD matchup') && checkMessageCategory(message)) {
+    const week = message.content.split(' ')[2];
+    if (!parseInt(week)) {
+      message.reply('Matchup count must be a number, try again!');
+      return;
+    }
+    message.channel.send(`OPEN DIV STATS ${week}`);
   }
 });
 
-client.login(`${process.env.TOKEN}`);
+function checkMessageCategory(message) {
+  const category = message.channel.parent.name;
+  return teams[category.toUpperCase()];
+}
+
+client.login(process.env.TOKEN);
