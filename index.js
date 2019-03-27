beb6c25210cbf3482957e700aba55f926f53494a
Drake Bott <bott.da@gmail.com>
2019-03-27 15:28:31 -0600

    Add try/catch block to message response

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const scraper = require('./utils/scraper');
+const { fetchTeamStats } = require('./utils/fetchStats');
 
 const teams = {
   '5280 ELITE': '5280 Elite',
@@ -21,23 +22,43 @@ client.on('message', async message => {
   if (message.author === client.user) {
     return;
   }
-  if (message.content.startsWith('!OD matchup') && checkMessageCategory(message)) {
-    const round = message.content.split(' ')[2];
-    if (!parseInt(round)) {
-      message.reply('Matchup count must be a number, try again!');
-      return;
+  if (
+    message.content.startsWith('!OD matchup') &&
+    checkMessageCategory(message)
+  ) {
+    try {
+      const round = message.content.split(' ')[2];
+      const team = teams[message.channel.parent.name.toUpperCase()];
+      if (!parseInt(round)) {
+        message.reply('Matchup count must be a number, try again!');
+        return;
+      }
+      const matchStats = await scraper.getRosters(team, round);
+      matchStats.roster = await fetchTeamStats(matchStats.roster);
+      matchStats.round = round;
+      const embed = createEmbed(matchStats, team);
+      message.channel.send({ embed });
+    } catch (error) {
+      console.log(error);
     }
-    const roster = await scraper.getRosters(
-      teams[message.channel.parent.name.toUpperCase()],
-      round,
-    );
-    message.channel.send(`OPEN DIV STATS ${roster}`);
   }
 });
 
 function checkMessageCategory(message) {
   const category = message.channel.parent.name;
   return teams[category.toUpperCase()];
+}
+
+function createEmbed(stats, team) {
+  const embed = new Discord.RichEmbed()
+    .setTitle(`${team} match #${stats.round} vs. ${stats.name}`)
+    .setColor(0x00AE86)
+    .setThumbnail('https://cdn.discordapp.com/attachments/546536122407190530/546536510309269514/artboard_1.png')
+  console.log(stats.roster);
+  stats.roster.forEach(player => {
+    embed.addField(player.player, `SR: ${player.rating}`);
+  });
+  return embed;
 }
 
 client.login(process.env.TOKEN);
