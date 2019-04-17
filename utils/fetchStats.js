b932a2d1c7caed0c867216b61f9aa52cb3f87be7
Drake Bott <bott.da@gmail.com>
2019-04-16 23:05:24 -0600

    Add error handling for fetching player SR

--- utils/fetchStats.js
+++ utils/fetchStats.js
@@ -4,16 +4,23 @@ const utf8 = require('utf8');
 function fetchTeamStats(team) {
   return Promise.all(
     team.map(async player => {
-      const tag = player.replace('#', '-').split(' ')[0];
-      const res = await fetch(
-        utf8.encode(`https://ow-api.com/v1/stats/pc/us/${tag}/profile`),
-      );
-      const stats = await res.json();
-      const rating = stats.rating || 'Private';
-      return {
-        player,
-        rating,
-      };
+      try {
+        const tag = player.replace('#', '-').split(' ')[0];
+        const res = await fetch(
+          utf8.encode(`https://ow-api.com/v1/stats/pc/us/${tag}/profile`),
+        );
+        const stats = await res.json();
+        const rating = stats.rating || 'Private';
+        return {
+          player,
+          rating,
+        };
+      } catch (error) {
+        return {
+          player,
+          rating: 'Private',
+        };
+      }
     }),
   );
 }
